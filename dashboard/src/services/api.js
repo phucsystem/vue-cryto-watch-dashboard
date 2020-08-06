5f73ed76bcf9559794901d2812bbf8879e14fd94
phuc dang <phucsystem@gmail.com>
2020-08-07 01:46:42 +0700

    Frist commit

---
+++ dashboard/src/services/api.js
@@ -0,0 +1,90 @@
+import WS from './ws'
+import defaultPair from '@/assets/defaultpair.json'
+
+export default class Api {
+    constructor(timeout = 5e3, maxAttempts = 5) {
+        this.timeout = timeout;
+        this.maxAttempts = maxAttempts;
+        this._baseUrl = 'wss://stream.cryptowat.ch/connect?apikey=' + 'JVDEG2PXBE9G6I98DEQ6';
+        this.subscription = {};
+        this.streams = {
+            ticker: (symbol) => `${symbol.toLowerCase()}@ticker`,
+        };
+    }
+
+    subscribe(cb, options) {
+        console.log(defaultPair);
+        let ws;
+        try {
+            let path = this._baseUrl;
+            if (this.subscription[path]) {
+                return this.subscription[path];
+            }
+            ws = new WS(path, {
+                timeout: this.timeout,
+                maxAttempts: this.maxAttempts,
+                binaryType: 'arraybuffer',
+                onopen: e => console.log('Connected!', e),
+                onmessage: e => {
+                    var enc = new TextDecoder("utf-8");
+                    var arr = new Int8Array(e.data);
+                    var jsonData = JSON.parse(enc.decode(arr));
+                    if (jsonData.authenticationResult && jsonData.authenticationResult.status === 'AUTHENTICATED') {
+                        const subscribe = function (conn, resources) {
+                            conn.send(JSON.stringify({
+                                subscribe: {
+                                    subscriptions: resources.map((resource) => {
+                                        return {streamSubscription: {resource: resource}}
+                                    })
+                                }
+                            }));
+                        }
+                        const resources = defaultPair.map(({resource}) => resource);
+                        subscribe(ws.conn, resources);
+                        return
+                    }
+
+                    console.log(jsonData);
+
+                    if(jsonData.marketUpdate){
+                        const currency = defaultPair.find(x => x.currencyPairId == jsonData.marketUpdate.market.currencyPairId);
+                        jsonData['symbol'] = currency.symbol;
+                        cb(jsonData);
+                    }
+                },
+                onreconnect: e => console.log('Reconnecting...', e),
+                onmaximum: e => console.log('Stop Attempting!', e),
+                onclose: e => {
+                    this.removeSubscription(e.endpoint)
+                },
+                onerror: e => console.log('Error:', e)
+            });
+            this.subscription[path] = ws
+        } catch (ex) {
+            console.log("Error :" + ex)
+        }
+    }
+
+    caller(fname, ...args) {
+        return this.streams[fname].call(null, ...args)
+    }
+
+    removeSubscription(endpoint) {
+        if (this.subscription[endpoint]) {
+            delete this.subscription[endpoint]
+        }
+    }
+
+    closeSubscription(type, isCombined = false, ...args) {
+        const endpoint = this.caller(type, ...args);
+        const path = this._baseUrl + endpoint;
+        const ws = this.subscription[path];
+        if (ws) {
+            ws.close(1000, "");
+        }
+    }
+
+    onTicker(options, eventHandler) {
+        return this.subscribe(eventHandler, options);
+    }
+}
---
+++ dashboard/src/views/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="content-box">
+    <CryptoBoard></CryptoBoard>
+  </div>
+</template>
+<script>
+import coins from '@/assets/group.json'
+import {subscribeSymbol} from '../services/binance'
+import {mapState} from 'vuex'
+import CryptoBoard from "@/views/CryptoBoard";
+
+export default {
+  name: 'dashboard',
+  data() {
+    return {
+      currencyList: coins,
+      quote: 'BNB',
+      quoteOptions: ['BNB', 'BTC', 'ETH', 'USDT'],
+      baseCurrency: {},
+    }
+  },
+  mounted() {
+    subscribeSymbol({});
+  },
+  computed: {
+    ...mapState(['currencies'])
+  },
+  components: {
+    CryptoBoard
+  },
+  methods: {
+    resetBase() {
+      this.baseCurrency = {}
+    },
+  }
+}
+</script>
