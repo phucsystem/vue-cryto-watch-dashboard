8bc535d6d032a2a8af38b8ef602352c19d11ae0c
phuc dang <phucsystem@gmail.com>
2020-08-07 01:48:07 +0700

    Cleanup

--- dashboard/src/layout/LayoutPage.vue
+++ dashboard/src/layout/LayoutPage.vue
@@ -4,7 +4,7 @@
             <button class="back-btn" v-if="currentPage === 'infoview'" @click="$router.push({ path: '/' })">
                 <i class="fa fa-angle-left fa-2x"></i>
             </button>
-            <span class="page-title">BINANCE CRYPTO DASHBOARD</span>
+            <span class="page-title">CRYPTO WATCH DASHBOARD</span>
         </header>
         <div class="page-container">
             <transition name="fade" mode="out-in">
